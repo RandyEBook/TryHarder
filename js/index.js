f2c7410af9c58b6f3f26297c06234cdf75f3c14c
RandyEBook <103864361+RandyEBook@users.noreply.github.com>
2023-06-19 19:14:31 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -210,7 +210,7 @@ function getRewards(currentAddr) {
         proper = res * res9
       
         proper1 = proper
-        proper2 = proper1 * (10 ** -16)
+        proper2 = proper1 * (10 ** -2)
         $("#YourRewards").text(proper2 + " CRO");
         console.log(proper2);
         })
